8a1b07944016cefe66ba46c20431ef4a8317f713
Nur <nuralam.rsc@gmail.com>
2025-06-23 17:27:38 +0600

    chore: cleanup
    chore: add logo

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,39 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Create an account',
 };
 
 const Register = () => {
-  return <>Register</>;
+  return (
+    <div>
+      <div>
+        <Link
+          href='/'
+          className='flex items-center gap-1'
+        >
+          <Image
+            alt='Cloudy'
+            src='/cloudy-light.svg'
+            width={64}
+            height={64}
+          />{' '}
+          <span className='text-xl font-bold'>Cloudy</span>
+        </Link>
+      </div>
+
+      <div>
+        <h2>Create an account</h2>
+        <p>
+          Register today and gain access to powerful tools that will supercharge
+          your ideas.
+        </p>
+
+        <form></form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
