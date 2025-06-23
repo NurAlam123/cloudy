99d83c02199bf88537c41c5845445edb9836e3a6
Nur <nuralam.rsc@gmail.com>
2025-06-23 16:08:55 +0600

    chore: move fonts directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { montserrat } from './fonts';
+import { montserrat } from '@/fonts';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
