1e5f360ef9e803d090048633d599365f72b8720b
Nur <nuralam.rsc@gmail.com>
2025-06-25 19:29:12 +0600

    fix(greetings): add the user name from auth

--- src/components/Greetings.tsx
+++ src/components/Greetings.tsx
@@ -1,10 +1,24 @@
 'use client';
 
+import getUser from '@/lib/getUser';
 import { motion } from 'motion/react';
+import { useEffect, useState } from 'react';
 
 const Greetings = () => {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    getUser().then((res) => {
+      if (!res) return;
+      setName(res.name);
+    });
+  }, [name]);
+
   return (
-    <div className='grid place-content-center h-full'>
+    <div
+      className='grid place-content-center h-full'
+      suppressHydrationWarning
+    >
       <h2 className='text-headline-large font-semibold text-center tracking-tight text-light-onSurfaceVariant dark:text-dark-onSurfaceVariant'>
         <motion.span
           initial={{
@@ -19,7 +33,7 @@ const Greetings = () => {
           }}
           className='bg-gradient-to-r from-teal-400 from-0% via-cyan-500 via-56% to-transparent to-75% bg-clip-text text-transparent bg-[length:350%_100%] bg-[100%_0]'
         >
-          Hello, Nur
+          Hello, {name || 'User'}
         </motion.span>
         <br />
         <motion.span
